dcc0ff75bcbe46984f94cd08c17a32127b553d5d
Jared Grippe <jared@deadlyicon.com>
2022-07-06 15:43:44 -0700

    WIP

---
+++ test/helpers/test.js
@@ -0,0 +1,90 @@
+const tape = require('tape')
+const tmp = require('tmp-promise')
+const fs = require('node:fs/promises')
+const HyperDHT = require('@hyperswarm/dht')
+const Vault = require('jlinx-vault')
+const { createSigningKeyPair } = require('jlinx-util')
+const JlinxHost = require('jlinx-host')
+const createJlinxHostHttpServer = require('jlinx-host/http-server')
+
+const JlinxClient = require('../../index.js')
+
+module.exports.test = function(name, fn, _tape = tape) {
+  return _tape(name, run)
+  async function run (t) {
+    const bootstrappers = []
+    const nodes = []
+
+    while (bootstrappers.length < 3) {
+      bootstrappers.push(new HyperDHT({ ephemeral: true, bootstrap: [] }))
+    }
+
+    const bootstrap = []
+    for (const node of bootstrappers) {
+      await node.ready()
+      bootstrap.push({ host: '127.0.0.1', port: node.address().port })
+    }
+
+    while (nodes.length < 3) {
+      const node = new HyperDHT({ ephemeral: false, bootstrap })
+      await node.ready()
+      nodes.push(node)
+    }
+
+    const tmpDirs = []
+    const newTmpDir = async () => {
+      const { path } = await tmp.dir()
+      const destroy = () => fs.rm(path, { recursive: true })
+      tmpDirs.push({ path, destroy })
+      return path
+    }
+
+    const jlinxHosts = []
+    const jlinxHostHttpServers = []
+    const createHost = async () => {
+      const jlinxHost = new JlinxHost({
+        storagePath: await newTmpDir(),
+        bootstrap,
+        url: `http://example.com`,
+        keyPair: createSigningKeyPair(),
+        vaultKey: Vault.generateKey()
+      })
+      jlinxHosts.push(jlinxHost)
+      await jlinxHost.ready()
+      const httpServer = createJlinxHostHttpServer(jlinxHost)
+      await httpServer.start()
+      jlinxHostHttpServers.push(httpServer)
+      return jlinxHost
+    }
+    while (jlinxHosts.length < 2) createHost()
+
+    const jlinxClients = []
+    const createClient = async () => {
+      const jlinxClient = new JlinxClient({
+        vaultPath: await newTmpDir(),
+        hostUrl: jlinxHost.url,
+        vaultKey: Vault.generateKey(),
+      })
+      jlinxClients.push(jlinxClient)
+      return jlinxClient
+    }
+
+    await fn(t, createClient)
+
+    destroy(jlinxClients)
+    destroy(jlinxHostHttpServers)
+    destroy(jlinxHosts)
+    destroy(tmpDirs)
+    destroy(bootstrappers)
+    destroy(nodes)
+  }
+}
+exports.test.only = (name, fn) => exports.test(name, fn, tape.only)
+exports.test.skip = (name, fn) => exports.test(name, fn, tape.skip)
+
+function destroy (...nodes) {
+  for (const node of nodes) {
+    if (Array.isArray(node)) destroy(...node)
+    else node.destroy()
+  }
+}
